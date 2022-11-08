65794aef941f618fe0768cf2a1978e29d4fd8ac2
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-11-08 19:47:16 +0300

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Utku Kaan Gülsoy on 8.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
